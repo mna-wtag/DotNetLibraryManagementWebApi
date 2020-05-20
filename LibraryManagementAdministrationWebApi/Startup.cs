f325e27234f21be47e1e5e4fe1f15f22633c8cfc
mna-wtag <mahfuz.nabi@welldev.com>
2020-05-20 14:05:48 +0600

    Dev Branch initial commit

--- LibraryManagementAdministrationWebApi/Startup.cs
+++ LibraryManagementAdministrationWebApi/Startup.cs
@@ -66,6 +66,7 @@ namespace LibraryManagementAdministrationWebApi
                 });
         }
 
+        //Testing dev branch
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
